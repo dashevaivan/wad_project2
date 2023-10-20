f804ce815e0ab68eeb9ea109cdb5d728b6f68071
dashevaivan <115436312+dashevaivan@users.noreply.github.com>
2023-10-20 18:21:10 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Latihan 2 PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class='grid text-center'>
+        <h1>Tugas 2</h1>
+    </div>
+
+    <div class="mb-3; container">
+    <label for="exampleFormControlInput1" class="form-label">Masukkan Angka</label>
+    <input type="integer" class="form-control" id="exampleFormControlInput1" placeholder="Contoh : 1">
+    </div>
+
+    <div class="container" style="margin-top: 10px ;">
+        <p>Masukkan Input Kedua</p>
+    </div>
+
+    <select class="form-select mb-1; container" style="padding: 7px;">
+    <option selected>Pilih Angka</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="3">4</option>
+    <option value="3">5</option>
+    <option value="3">6</option>
+    <option value="3">7</option>
+    <option value="3">8</option>
+    </select>
+
+    <div class="container">
+    <button type="button" class="btn btn-success" style="margin-top: 15px;">Hitung</button>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
